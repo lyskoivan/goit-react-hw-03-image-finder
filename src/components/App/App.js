688c9318325005e4f22c3180a98598a430bae9fc
lyskoivan <lysko1999i@gmail.com>
2020-01-25 02:06:40 +0200

    add api url

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    page: 1,
+    query: ""
+  };
+  render() {
+    return (
+      <div>
+        <h1>Something</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/services/api.js
@@ -0,0 +1,5 @@
+const API_KEY = "15026161-e432bf02d9d2367e14c73284c";
+
+const URL = "https://pixabay.com/api/";
+
+export default { API_KEY, URL };
